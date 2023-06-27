44c75f66e89e1de641d22dc3c72880e8585c853a
yaroslav7516 <yaroslav.sportcrossua@gmail.com>
2023-06-28 02:05:10 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,36 @@
 $(function () {
+
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--activ');
+    $(this).addClass('shop-content__filter-btn--activ');
+  });
+
+  $('.button-list').on('click', function () {
+    $('.product-item').addClass('product-item--list');
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list');
+  });
+
+  
+
+  $('.select-style').styler();
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+        },
+  });
+
+
   
   $('.top-slider__inner').slick({
     dots: true,
